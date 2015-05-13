f8b13848c48f28b68585e6260146b183612c7897
Andres Cabrera <andrescabsi@gmail.com>
2015-05-12 22:46:49 -0500

    Footer - home - metodo - contacto

--- wp-content/themes/CompulearningOrg/footer.php
+++ wp-content/themes/CompulearningOrg/footer.php
@@ -5,22 +5,26 @@
 	/////////////////////////////////////////////////////////////// -->
 	<footer class="footer">
 		<section class="container">
-			<ul class="container_info col-xs-4">
-				<li><strong>Dirección:</strong> Cra 18 N° 78 - 40 Of. 403 - Bogotá - Colombia </li>
-				<li><strong>Correo Electrónico:</strong> info@compulearning.edu.co </li>
-				<li><strong>Teléfonos:</strong> +571 621 9327 - 621 9303 - 621 8317 </li>
-				<li><strong>Móvil:</strong> +57 312 431 3742</li>
-			</ul>
-			<!-- <ul class="container_bottom_nav col-xs-8">
-				<li><a href="contactenos.html">CONTÁCTENOS</a></li>
-				<li><a href="http://compulearning2.azurewebsites.net/certificados.php">CERTIFICADOS</a></li>
-				<li><a href="http://compulearning2.azurewebsites.net/instituciones-educativas.html">INST. EDUCATIVAS</a></li>
-				<li><a href="http://compulearning2.azurewebsites.net/ingresar.php">PROFESORES</a></li>
-				<li><a href="http://www.compulearning.edu.co/CompulearningEduCo/cursos-presenciales-capacitacion-empresarial.html">PRESENCIALES</a></li>
-				<li><a href="metodo.html">MÉTODO</a></li>
-				<li><a href="nosotros.html">NOSOTROS</a></li>
-			</ul> -->
-			<?php wp_nav_menu( array( 'theme_location' => 'pie-pagina' ) ); ?>
+			<div class="container_info col-xs-4">
+				<?php
+					$args = array(
+						'post_type' => 'contenidos',
+						'name' => 'footer-informacion'
+					);
+					$query = new WP_Query( $args );
+				 ?>
+				<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+				<?php else: ?>
+				<?php endif; ?>
+				<?php rewind_posts(); ?><?php wp_reset_query(); ?>	
+			</div>
+			<div class="container_bottom_nav col-xs-8">
+				<?php wp_nav_menu( array( 'theme_location' => 'pie-pagina' ) ); ?>
+			</div>
+
+			
 			<div class="container_credits col-xs-12 text-right">
 				<a target="_blank" href="http://www.innov.com.co">powered by <strong>INNOV</strong></a>
 			</div>
@@ -32,3 +36,4 @@
 	<script src="<?php bloginfo('template_directory'); ?>/js/index.js"></script>
 </body>
 </html>
+
--- wp-content/themes/CompulearningOrg/page-inicio.php
+++ wp-content/themes/CompulearningOrg/page-inicio.php
@@ -5,294 +5,148 @@ CONTENIDO
 /////////////////////////////////////////////////////////////// -->
 <section class="container_slider col-xs-12">
 	<div class="container">
-			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-			<!-- Indicators -->
-			<ol class="carousel-indicators">
-				<li data-target="#carousel-example-generic" data-slide-to="0" class="cursos active">
-					<div>Cursos<span></span></div>
-				</li>
-				<li data-target="#carousel-example-generic" data-slide-to="1" class="basico">
-					<div>Básico<span></span></div>
-				</li>
-				<li data-target="#carousel-example-generic" data-slide-to="2" class="graficas">
-					<div>Gráficas<span></span></div>
-				</li>
-				<li data-target="#carousel-example-generic" data-slide-to="3" class="tablas">
-					<div>Tablas Dinámicas<span></span></div>
-				</li>
-				<li data-target="#carousel-example-generic" data-slide-to="4" class="macros">
-					<div>Macros<span></span></div>
-				</li>
-			</ol>
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-example-generic" data-slide-to="0" class="cursos active">
+				<div>Cursos<span></span></div>
+			</li>
+			<li data-target="#carousel-example-generic" data-slide-to="1" class="basico">
+				<div>Básico<span></span></div>
+			</li>
+			<li data-target="#carousel-example-generic" data-slide-to="2" class="graficas">
+				<div>Gráficas<span></span></div>
+			</li>
+			<li data-target="#carousel-example-generic" data-slide-to="3" class="tablas">
+				<div>Tablas Dinámicas<span></span></div>
+			</li>
+			<li data-target="#carousel-example-generic" data-slide-to="4" class="macros">
+				<div>Macros<span></span></div>
+			</li>
+		</ol>
 
-			<!-- Wrapper for slides -->
-			<div id="main_courses_slider" class="carousel-inner" role="listbox">
-				
-
-
-				<!-- ///////////////////////////////////////////////////////////////
-				SLIDER INICIAL
-				/////////////////////////////////////////////////////////////// -->
+		<!-- Wrapper for slides -->
+		<div id="main_courses_slider" class="carousel-inner" role="listbox">
+			<!-- ///////////////////////////////////////////////////////////////
+			SLIDER BIENVENIDA
+			/////////////////////////////////////////////////////////////// -->
+			<?php
+				$args = array(
+					'post_type' => 'contenidos',
+					'name' => 'slide-bienvenida'
+				);
+				$query = new WP_Query( $args );
+			 ?>
+			<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 				<div class="item active" data-hash="graficas">
 					<div class="descriptive_txt col-xs-7">
 						<div class="slider_content_wrapper">
-							<h1>Cursos Virtuales Multimedia</h1>
-							<h3>12 Video-Lecciones con Actividades Prácticas</h3>
+							<h1><?php the_field('titulo'); ?></h1>
+							<h3><?php the_field('subtitulo'); ?></h3>
 						</div>
 					</div>
 					<div class="descriptive_img col-xs-5">
-						<img src="img/cursos/package.png" alt="">
+						<img src="<?php the_field('imagen'); ?>" alt="">
 					</div>
 				</div>
-				<!-- ///////////////////////////////////////////////////////////////
-				SLIDER BASICO
-				/////////////////////////////////////////////////////////////// -->
-				<div class="item" data-hash="tablas">
-					<h2 class="col-xs-12">EXCEL BÁSICO <span>Tips y trucos …</span></h2>
+			<?php endwhile; ?>
+			<?php else: ?>
+			<?php endif; ?>
+			<?php rewind_posts(); ?><?php wp_reset_query(); ?>	
+			<!-- ///////////////////////////////////////////////////////////////
+			SLIDES
+			/////////////////////////////////////////////////////////////// -->
+			<?php
+				$args = array(
+					'post_type' => 'slider_inicio',
+					'posts_per_page' => -1
+				);
+				$query = new WP_Query( $args );
+			 ?>
+			<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+
+				<div class="item" data-hash="<?php the_field('texto_boton'); ?>">
+					<h2 class="col-xs-12"><?php the_title(); ?> <span><?php the_field('subtitulo') ?></span></h2>
 					<div class="col-xs-8">
-						<p class="price">USD $29</p>
-						<p class="duracion_curso basico">
+						<p class="price"><?php the_field('precio') ?></p>
+						<p class="duracion_curso <?php the_field('texto_boton'); ?>">
 							<span class="time_icon"></span>
-							<strong>Duración:</strong> 12 a 18 horas
+							<strong>Duración:</strong> <?php the_field('duracion') ?>
 						</p>
-						<p class="video_curso basico">
+						<p class="video_curso <?php the_field('texto_boton'); ?>">
 							<span class="video_icon"></span>
-							Más de 2 horas de Video en alta resolución.
+							<?php the_field('horas_video') ?>
 						</p>
-						<a class="demo_video" href="http://compulearning2.azurewebsites.net/videos.php?id=lo38xPB7v4c">Ver Video Demo </a>
+						<a class="demo_video" href="<?php the_field('video_demo') ?>">Ver Video Demo </a>
 						<h3 class="downloads_label">Descargas sin restricciones</h3>
-						<a class="downloads basico" href="http://www.excelvirtual.org/Archivos/ORG/Temario%20Detallado%20Basico%20ORg.pdf">
+						<a class="downloads <?php the_field('texto_boton'); ?>" href="<?php the_field('temario') ?>">
 							<span class="icon_download"></span>Temario Detallado
 						</a>
-						<a class="downloads basico" href="http://demo-curso-excel-basico-tips-y-trucos.softonic.com/">
+						<a class="downloads <?php the_field('texto_boton'); ?>" href="<?php the_field('curso_demostracion') ?>">
 							<span class="icon_download"></span>Curso Demostración
 						</a>
-						<a class="downloads basico" href="http://www.excelvirtual.org/Archivos/ORG/ArchivosTrabajo/ArchivosCursoBasico.rar">
+						<a class="downloads <?php the_field('texto_boton'); ?>" href="<?php the_field('archivos_trabajo') ?>">
 							<span class="icon_download"></span>Archivos de Trabajo
 						</a>
 					</div><!--End_left_side_col-->
 					<div class="col-xs-4">
-					  	<img class="product_img pull-right" src="img/cursos/basico.png" alt="">
-					  	<a class="buy_btn_link pull-right" href="compra-excel-basico.html">
+					  	<img class="product_img pull-right" src="<?php the_field('imagen_curso'); ?>" alt="">
+					  	<a class="buy_btn_link pull-right" href="<?php the_field('compra_curso') ?>">
 					  		<div class="buy_btn pull-right">Comprar Curso</div>
 					  	</a>
 					</div><!--End_right_side_col-->
 				</div><!--End_tab_1-->
-				<!-- ///////////////////////////////////////////////////////////////
-				SLIDER GRÁFICAS
-				/////////////////////////////////////////////////////////////// -->
-				<div class="item" data-hash="macros">
-					<h2 class="col-xs-12">GRÁFICAS EN EXCEL <span>GRAFICACIÓN DESCRIPTIVA</span></h2>
-					<div class="col-xs-8">
-						<p class="price">USD $39</p>
-						<p class="duracion_curso graficas">
-							<span class="time_icon"></span>
-							<strong>Duración:</strong> 12 a 18 horas
-						</p>
-						<p class="video_curso graficas">
-							<span class="video_icon"></span>
-							Más de 2 horas de Video en alta resolución.
-						</p>
-						<a class="demo_video" href="http://compulearning2.azurewebsites.net/videos.php?id=BDYv1ifPMyc">Ver Video Demo </a>
-						<h3 class="downloads_label">Descargas sin restricciones</h3>
-						<a class="downloads graficas" href="http://www.excelvirtual.org/Archivos/ORG/Temario%20detallado%20Graficas%20Org.pdf">
-							<span class="icon_download"></span>Temario Detallado
-						</a>
-						<a class="downloads graficas" href="http://demo-curso-excel-para-la-graficacion-descriptiva.softonic.com/">
-							<span class="icon_download"></span>Curso Demostración
-						</a>
-						<a class="downloads graficas" href="http://www.excelvirtual.org/Archivos/ORG/ArchivosTrabajo/ArchivosCursoGraficas.rar">
-							<span class="icon_download"></span>Archivos de Trabajo
-						</a>
-					</div><!--End_left_side_col-->
-					<div class="col-xs-4">
-					  	<img class="product_img pull-right" src="img/cursos/graficas.png" alt="">
-					  	<a class="buy_btn_link pull-right" href="compra-excel-graficas.html">
-					  		<div class="buy_btn pull-right">Comprar Curso</div>
-					  	</a>
-					</div><!--End_right_side_col-->
-				</div><!--End_tab_2-->
-				<!-- ///////////////////////////////////////////////////////////////
-				SLIDER TABLAS DINÁMICAS
-				/////////////////////////////////////////////////////////////// -->
-				<div class="item" data-hash="cursos">
-					<h2 class="col-xs-12">Tablas Dinámicas <span>EXCEL PARA EL ANÁLI…</span></h2>
-					<div class="col-xs-8">
-						<p class="price">USD $49</p>
-						<p class="duracion_curso tablas">
-							<span class="time_icon"></span>
-							<strong>Duración:</strong> 12 a 18 horas
-						</p>
-						<p class="video_curso tablas">
-							<span class="video_icon"></span>
-							Más de 2 horas de Video en alta resolución.
-						</p>
-						<a class="demo_video" href="http://compulearning2.azurewebsites.net/videos.php?id=neeSY1AA9Ak">Ver Video Demo </a>
-						<h3 class="downloads_label">Descargas sin restricciones</h3>
-						<a class="downloads tablas" href="http://www.excelvirtual.org/Archivos/ORG/Temario%20detallado%20TD%20Org.pdf">
-							<span class="icon_download"></span>Temario Detallado
-						</a>
-						<a class="downloads tablas" href="http://demo-curso-analisis-con-tablas-dinamicas-en-excel.softonic.com/">
-							<span class="icon_download"></span>Curso Demostración
-						</a>
-						<a class="downloads tablas" href="http://www.excelvirtual.org/Archivos/ORG/ArchivosTrabajo/ArchivosCursoTablasdinamicas.rar">
-							<span class="icon_download"></span>Archivos de Trabajo
-						</a>
-					</div><!--End_left_side_col-->
-					<div class="col-xs-4">
-					  	<img class="product_img pull-right" src="img/cursos/tablas.png" alt="">
-					  	<a class="buy_btn_link pull-right" href="compra-excel-tablas-dinamicas.html">
-					  		<div class="buy_btn pull-right">Comprar Curso</div>
-					  	</a>
-					</div><!--End_right_side_col-->
-				</div><!--End_tab_3-->
-				<!-- ///////////////////////////////////////////////////////////////
-				SLIDER MACROS
-				/////////////////////////////////////////////////////////////// -->
-				<div class="item" data-hash="basico">
-					<h2 class="col-xs-12">Macros en Excel <span>Con Visual Basic…</span></h2>
-					<div class="col-xs-8">
-						<p class="price">USD $59</p>
-						<p class="duracion_curso macros">
-							<span class="time_icon"></span>
-							<strong>Duración:</strong> 12 a 24 horas
-						</p>
-						<p class="video_curso macros">
-							<span class="video_icon"></span>
-							Más de 2 horas de Video en alta resolución.
-						</p>
-						<a class="demo_video" href="http://compulearning2.azurewebsites.net/videos.php?id=gsG-q7kcm4A">Ver Video Demo </a>
-						<h3 class="downloads_label">Descargas sin restricciones</h3>
-						<a class="downloads macros" href="http://www.excelvirtual.org/Archivos/ORG/Temario%20detallado%20Macros%20Org.pdf">
-							<span class="icon_download"></span>Temario Detallado
-						</a>
-						<a class="downloads macros" href="http://curso-de-macros-en-excel-con-visual-basic---demo.softonic.com/">
-							<span class="icon_download"></span>Curso Demostración
-						</a>
-						<a class="downloads macros" href="http://www.excelvirtual.org/Archivos/ORG/ArchivosTrabajo/ArchivosCursoMacros.rar">
-							<span class="icon_download"></span>Archivos de Trabajo
-						</a>
-					</div><!--End_left_side_col-->
-					<div class="col-xs-4">
-					  	<img class="product_img pull-right" src="img/cursos/macros.png" alt="">
-					  	<a class="buy_btn_link pull-right" href="compra-excel-macros.html">
-					  		<div class="buy_btn pull-right">Comprar Curso</div>
-					  	</a>
-					</div><!--End_right_side_col-->
-				</div><!--End_tab_4-->
 
+			<?php endwhile; ?>
+			<?php else: ?>
+				<p>No se encontraron menus.</p>
+			<?php endif; ?>
+			<?php rewind_posts(); ?><?php wp_reset_query(); ?>
 
+		</div><!-- end_carousel_inner -->
 
-
-			</div><!-- end_carousel_inner -->
-
-			<!-- Controls -->
-			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-			</a>
-			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-			</a>
-			</div>
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+		</a>
+		</div>
 	</div>
 </section>
-
-
-
-
-
-
 
 		<article class="container_testimonials col-xs-12">
 			<div class="container">
 				<!-- Testimonial Slider-->
 				<div id="testimonials_slider">
-					<div class="item">
-					<!-- ///////////////////////////////////////////////////////////////
-					TESTIMONIO 1
-					/////////////////////////////////////////////////////////////// -->
-						<div class="container_image_testimonial col-xs-4">
-							<a target="_blank" href="https://www.youtube.com/user/elbesodelhorror"><img src="img/testimonial/testimonial1.jpg" alt=""></a>
+					<?php
+						$args = array(
+							'post_type' => 'testimonios',
+							'posts_per_page' => -1
+						);
+						$query = new WP_Query( $args );
+					 ?>
+					<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+
+						<div class="item">
+							<div class="container_image_testimonial col-xs-4">
+								<a target="_blank" href="<?php the_field('link_testimonio') ?>"><img src="<?php the_field('imagen'); ?>" alt=""></a>
+							</div>
+							<div class="container_txt_testimonial col-xs-8">
+								<h3><a target="_blank" href="https://www.youtube.com/user/elbesodelhorror"><?php the_field('nombre_nickname'); ?></a></h3>
+								<p><?php the_field('testimonio'); ?></p>
+							</div><!--END TESTIMONIAL-->
 						</div>
-						<div class="container_txt_testimonial col-xs-8">
-							<h3><a target="_blank" href="https://www.youtube.com/user/elbesodelhorror">Ani Bal</a></h3>
-							<p>Gracias Masterexcel.... cumplio su proposito...... espero seguir aprendiendo, es muy sencillo el lenguaje graficos y explicaciones ojala pronto llegue la publicidad para que ganes buena lana.</p>
-						</div><!--END TESTIMONIAL-->
-					</div>
-					<div class="item">
-					<!-- ///////////////////////////////////////////////////////////////
-					TESTIMONIO 2
-					/////////////////////////////////////////////////////////////// -->
-						<div class="container_image_testimonial col-xs-4">
-							<a target="_blank" href="https://www.youtube.com/channel/UCveA4yGAONMDmYOteGUSMFQ"><img src="img/testimonial/testimonial2.jpg" alt=""></a>
-						</div>
-						<div class="container_txt_testimonial col-xs-8">
-							<h3><a target="_blank" href="https://www.youtube.com/channel/UCveA4yGAONMDmYOteGUSMFQ">Giovanny Fonseca</a></h3>
-							<p>Gracias estimado maestro su vídeo es excelente, me ha sido de gran provecho, lo felicito por su estrategia pedagógica.</p>
-						</div><!--END TESTIMONIAL-->
-					</div>
-					<div class="item">
-					<!-- ///////////////////////////////////////////////////////////////
-					TESTIMONIO 3
-					/////////////////////////////////////////////////////////////// -->
-						<div class="container_image_testimonial col-xs-4">
-							<a target="_blank" href="https://www.youtube.com/user/tirsoandres"><img src="img/testimonial/testimonial3.jpg" alt=""></a>
-						</div>
-						<div class="container_txt_testimonial col-xs-8">
-							<h3><a target="_blank" href="https://www.youtube.com/user/tirsoandres">Tutirjel</a></h3>
-							<p>Excelente, es un gran aporte a los que queremos profundizar le estudio de Excel, los felicito.﻿﻿</p>
-						</div><!--END TESTIMONIAL-->
-					</div>
-					<div class="item">
-					<!-- ///////////////////////////////////////////////////////////////
-					TESTIMONIO 4
-					/////////////////////////////////////////////////////////////// -->
-						<div class="container_image_testimonial col-xs-4">
-							<a target="_blank" href="https://www.youtube.com/user/jome221"><img src="img/testimonial/testimonial4.jpg" alt=""></a>
-						</div>
-						<div class="container_txt_testimonial col-xs-8">
-							<h3><a target="_blank" href="https://www.youtube.com/user/jome221">Jorge Antonio Medel Terrones</a></h3>
-							<p>Excelente, es un gran aporte a los que queremos profundizar el estudio de Excel, los felicito.</p>
-						</div><!--END TESTIMONIAL-->
-					</div>
-					<div class="item">
-					<!-- ///////////////////////////////////////////////////////////////
-					TESTIMONIO 5
-					/////////////////////////////////////////////////////////////// -->
-						<div class="container_image_testimonial col-xs-4">
-							<a target="_blank" href="https://www.youtube.com/user/josedavid660"><img src="img/testimonial/testimonial5.jpg" alt=""></a>
-						</div>
-						<div class="container_txt_testimonial col-xs-8">
-							<h3><a target="_blank" href="https://www.youtube.com/user/josedavid660">jose david Aguilar</a></h3>
-							<p>Muy buenas tus instrucciones en el vídeo gracias&nbsp; por compartir tus conocimientos</p>
-						</div><!--END TESTIMONIAL-->
-					</div>
-					<div class="item">
-					<!-- ///////////////////////////////////////////////////////////////
-					TESTIMONIO 6
-					/////////////////////////////////////////////////////////////// -->
-						<div class="container_image_testimonial col-xs-4">
-							<a target="_blank" href="http://www.youtube.com/user/TodoCivil"><img src="img/testimonial/testimonial6.jpg" alt=""></a>
-						</div>
-						<div class="container_txt_testimonial col-xs-8">
-							<h3><a target="_blank" href="http://www.youtube.com/user/TodoCivil">TodoCivil</a></h3>
-							<p>Excelente, muchas gracias! Esto nos sirve a muchos ingenieros!</p>
-						</div><!--END TESTIMONIAL-->
-					</div>
-					<div class="item">
-					<!-- ///////////////////////////////////////////////////////////////
-					TESTIMONIO 7
-					/////////////////////////////////////////////////////////////// -->
-						<div class="container_image_testimonial col-xs-4">
-							<a target="_blank" href="http://www.youtube.com/user/sistemfree"><img src="img/testimonial/testimonial7.jpg" alt=""></a>
-						</div>
-						<div class="container_txt_testimonial col-xs-8">
-							<h3><a target="_blank" href="http://www.youtube.com/user/sistemfree">Freeman Skifreehace</a></h3>
-							<p>Excelente Explicacion gracias por su tiempo, a seguir con el siguiente video. Saludos.!</p>
-						</div><!--END TESTIMONIAL-->
-					</div>
+
+					<?php endwhile; ?>
+					<?php else: ?>
+						<p>No se encontraron menus.</p>
+					<?php endif; ?>
+					<?php rewind_posts(); ?><?php wp_reset_query(); ?>
+
 				</div><!--End slider-->
 				<div class="testimonials_main_title_container">
 					<h3 class="testimonials_main_title">TESTIMONIOS <span>EN YOUTUBE</span></h3>
